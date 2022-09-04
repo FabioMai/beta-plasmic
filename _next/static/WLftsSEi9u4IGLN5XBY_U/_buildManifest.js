b53c7a35662a05493422e971a958d341822a0ae3
FabioMai <fabio.maienschein@gmail.com>
2022-09-04 20:23:37 +0000

    Deploying to gh-pages from @ FabioMai/beta-plasmic@c4474241d58a0511dd53cebafd127fd7f2107ee7 🚀

---
+++ _next/static/WLftsSEi9u4IGLN5XBY_U/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-65671d205c14eb78.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/plasmic-host":["static/chunks/pages/plasmic-host-2d360d66b0ba7ade.js"],sortedPages:["/","/_app","/_error","/plasmic-host"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
